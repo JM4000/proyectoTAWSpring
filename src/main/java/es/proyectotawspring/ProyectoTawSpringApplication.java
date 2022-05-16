44081ff4de60b184468d48fe29644221800f4ea5
JM4000 <juanmarcosmega@gmail.com>
2022-05-16 18:05:57 +0200

    Initial commit

---
+++ src/main/java/es/proyectotawspring/ProyectoTawSpringApplication.java
@@ -0,0 +1,13 @@
+package es.proyectotawspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTawSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoTawSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/proyectotawspring/ProyectoTawSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package es.proyectotawspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTawSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
